d146e58064299944ffa0201b3bf675c5fec0dd34
utsavnaik <naik.utsav@gmail.com>
2020-04-16 10:00:05 +0530

    memory management unsubscribe all observable

--- src/app/employee/employeelist/employeelist.component.ts
+++ src/app/employee/employeelist/employeelist.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
-import { Observable, merge, fromEvent } from 'rxjs';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
+import { Observable, merge, fromEvent, Subscription } from 'rxjs';
 import { Employee, EmployeeApiModel, EmployeeModel } from '../models/employee.model';
 import { EmployeeService } from '../services/employee.service';
 import {MatPaginator} from '@angular/material/paginator';
@@ -13,12 +13,12 @@ import { tap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
   templateUrl: './employeelist.component.html',
   styleUrls: ['./employeelist.component.css']
 })
-export class EmployeelistComponent implements AfterViewInit, OnInit {
+export class EmployeelistComponent implements AfterViewInit, OnInit,OnDestroy {
   field = 'id';
   dataSource:any;
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'gender', 'contact'];
   allEmployees: Observable<EmployeeModel[]>;  
-  
+  subscriptions: Subscription[] = []
   @ViewChild('input',{static:true}) input: ElementRef;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort,{static:true}) sort: MatSort;
@@ -39,19 +39,19 @@ export class EmployeelistComponent implements AfterViewInit, OnInit {
 
   performSort() {
     this.sort.direction = 'asc';
-    this.sort.sortChange.subscribe((ele) => {
+    this.subscriptions.push(this.sort.sortChange.subscribe((ele) => {
       this.paginator.pageIndex = 0;
       this.field = ele.active;
       this.loadAllEmployees();
-     });
+     }));
   }
 
   pageChange() {
-    this.paginator.page
+    this.subscriptions.push(this.paginator.page
     .pipe(
         tap(() => this.loadAllEmployees())
     )
-    .subscribe();
+    .subscribe());
   }
 
   onRowClicked(row) {
@@ -59,7 +59,7 @@ export class EmployeelistComponent implements AfterViewInit, OnInit {
   }
 
   performSearch() {
-    fromEvent(this.input.nativeElement,'keyup')
+    this.subscriptions.push(fromEvent(this.input.nativeElement,'keyup')
             .pipe(
                 debounceTime(150),
                 distinctUntilChanged(),
@@ -68,13 +68,17 @@ export class EmployeelistComponent implements AfterViewInit, OnInit {
                     this.loadAllEmployees();
                 })
             )
-            .subscribe();
+            .subscribe());
   }
 
    loadAllEmployees() {  
-    this.employeeService.getAllEmployee(this.field,this.sort.direction,this.paginator.pageIndex, this.paginator.pageSize,this.input.nativeElement.value).subscribe((data)=>{
+    this.subscriptions.push(this.employeeService.getAllEmployee(this.field,this.sort.direction,this.paginator.pageIndex, this.paginator.pageSize,this.input.nativeElement.value).subscribe((data)=>{
       this.dataSource = new MatTableDataSource<EmployeeModel>(data);
-    });
-  } 
+    }));
+  }
+  
+  ngOnDestroy(): void {
+     this.subscriptions.forEach((subscription)=> subscription.unsubscribe());
+  }
 
 }
